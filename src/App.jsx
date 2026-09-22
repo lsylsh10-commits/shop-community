f433c1a59bfa907fdaad189d5f05a4bb66d56b83
lsylsh10-commits <lsylsh10@gmail.com>
2026-09-22 09:58:40 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>저희 쇼핑몰 입니다</h1>
+    </>
+  )
+}
+
+export default App
